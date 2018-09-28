93913a7402e5cc737f9c8038ece927f9d943eef5
Andy Gayton <andy@thecablelounge.com>
2018-09-28 15:05:16 -0700

    new minimal www setup

---
+++ w2/webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        options: {
+          emitError: true,
+          failOnError: true
+        },
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+
+  output: {
+    filename: 'bundle-[contenthash].js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({template: 'src/index.html'})
+  ]
+}
